f41a006e1fe8905d5b249ab6e64e165f0f9b6b1a
Aryanparashara <aryanparashara01@gmail.com>
2025-08-27 22:40:46 +0530

    Create rivision.cpp

---
+++ rivision.cpp
@@ -0,0 +1,76 @@
+/*#include<iostream>
+int main()
+{
+  int a,b,c;
+  std::cout<<"enter two nummbers";
+  std::cin>>a>>b;
+  c=a+b;
+  std::cout<<"\n sum is "<<c;
+}*/
+
+/*#include<iostream>
+int main()
+{
+  int age;
+  std::cout << "\n enter the age of person ";
+  std::cin  >> age;
+  if(age>=18)
+  {
+    std::cout << "\n eligible ";
+  }
+  else
+  {
+    std::cout << "\n not eligible ";
+  }
+}*/
+
+/*#include<iostream>
+int main()
+{
+  int per;
+  std::cout << "\n enter the percentage ";
+  std::cin  >> per;
+  if(per>=90)
+  {
+    std::cout << "\n eligible for scholarship ";
+  }
+  else
+  {
+    std::cout << "\n not eligible for scholarship ";
+  }
+}*/
+
+/*#include<iostream>
+int main()
+{
+  int a,b;
+  std::cout << "\n enter two no.s ";
+  std::cin  >> a >> b;
+  if(a>b)
+  {
+    std::cout << "\n a is  greater ";
+  }
+  else 
+  {
+    std::cout << "\n b is greater ";
+  }
+}*/
+
+/*#include<iostream>
+int main()
+{
+  int bill,finalbill,discount;
+  std::cout << "\n enter the bill amount ";
+  std::cin  >> bill;
+  if(bill>=5000)
+  {
+    discount=(bill*10)/100;
+    std::cout << "\n discount is "<<discount;
+    finalbill=bill-discount;
+    std::cout << "\n finalbill is "<<finalbill;
+  }
+  else
+  {
+    std::cout << "\n No discount ";
+  }
+}*/
