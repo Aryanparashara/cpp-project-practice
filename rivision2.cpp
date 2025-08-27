da501fe37514c9904d4574a94736daa0dc709850
Aryanparashara <aryanparashara01@gmail.com>
2025-08-27 22:41:31 +0530

    Create rivision2.cpp

---
+++ rivision2.cpp
@@ -0,0 +1,75 @@
+/*#include<iostream>
+int main()
+{
+  int a,b,c,d,e,f;
+  std::cout<<"\n enter 2 no.s";
+  std::cin>>a>>b;
+  c=a+b;
+  std::cout<<"\n sum is "<<c;
+  d=a-b;
+  std::cout<<"\n sub is "<<d;
+  e=a*b;
+  std::cout<<"\n product is "<<e;
+  f=a/b;
+  std::cout<<"\n division is "<<f;
+}*/
+/*#include<iostream>
+int main()
+{
+  int a,b,c,d,e,f;
+  std::cout<<"\n enter marks of hindi";
+  std::cin>>a;
+  std::cout<<"\n enter marks of english";
+  std::cin>>b;
+  std::cout<<"\n enter marks of maths";
+  std::cin>>c;
+  std::cout<<"\n enter marks of science";
+  std::cin>>d;
+  std::cout<<"\n enter marks of arts";
+  std::cin>>e;
+  f=a+b+c+d+e;
+  std::cout<<"\n sum is "<<f;
+}*/
+/*#include<iostream>
+int main()
+{
+  int r,h,voc;
+  std::cout<<"\n enter the radius";
+  std::cin>>r;
+  std::cout<<"\n enter the height";
+  std::cin>>h;
+  voc=(3.14*r*r*h)/3;
+  std::cout<<"\n volume of cone is "<<voc;
+}*/
+/*#include<iostream>
+int main()
+{
+  int p,r,t,si;
+  std::cout<<"\n enter the principle amount";
+  std::cin>>p;
+  std::cout<<"\n enter the rate";
+  std::cin>>r;
+  std::cout<<"\n enter the time";
+  std::cin>>t;
+  si=(p*r*t)/100;
+  std::cout<<"\n simple interest is "<<si;
+}*/
+/*#include<iostream>
+int main()
+{
+  int basic,tada,hra,pf,medical,net,gross;
+  std::cout<<"\n enter the basic salary";
+  std::cin>>basic;
+  tada=(basic*30)/100;
+  std::cout<<"\n tada is"<<tada;
+  hra=(basic*50)/100;
+  std::cout<<"\n hra is "<<hra;
+  medical=(basic*20)/100;
+  std::cout<<"\n medical is "<<medical;
+  pf=(basic*15)/100;
+  std::cout<<"\n pf is "<<pf;
+  net=basic+hra+tada+medical+pf;
+  std::cout<<"\n net salary is "<<net;
+  gross=net-pf;
+  std::cout<<"\n gross salary is "<<gross;
+}*/
