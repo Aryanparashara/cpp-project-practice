73782c9b691ca31b01378b8be1a83f1bd6ebf655
Aryanparashara <aryanparashara01@gmail.com>
2025-09-07 21:17:51 +0530

    Create rivision4.cpp

---
+++ rivision4.cpp
@@ -0,0 +1,45 @@
+/*#include<iostream>
+int main()
+{
+  int a,b,c,d,e,f,g,per;
+  std::cout<<"\n CENTRAL BOARD OF SECONDARY EDUCATION ";
+  std::cout<<"\n                2025-27               ";
+  std::cout<<"\n ==================================== ";
+  std::cout<<"\n    sub     sub     max     marks     ";
+  std::cout<<"\n    name    code    marks   obtained  ";
+  std::cout<<"\n ==================================== ";
+  std::cout<<"\n    Math    101     100     ";
+  std::cin>>a;
+  std::cout<<"\n   Engilsh  102     100     ";
+  std::cin>>b;
+  std::cout<<"\n    SST     103     100     ";
+  std::cin>>c;
+  std::cout<<"\n    Hindi   104     100     ";
+  std::cin>>d;
+  std::cout<<"\n   Science  105     100     ";
+  std::cin>>e;
+  std::cout<<"\n   Computer 106     100     ";
+  std::cin>>f;
+  std::cout<<"\n ==================================== ";
+  g=a+b+c+d+e+f;
+  std::cout<<"\n  Total marks out of 600 =  "<<g;
+  std::cout<<"\n ==================================== ";
+  per=(100*g)/600;
+  std::cout<<"\n  percentage is "<<per;
+  if(per>=90)
+  {
+    std::cout<<"\n  You will get 100per. scholarship ";
+  }
+  else if ((per>=60)&&(per<90))
+  {
+    std::cout<<"\n  You will get 60per. scholarship ";
+  }
+  else if ((per>=40)&&(per<=60))
+  {
+    std::cout<<"\n  You will get 40per. scholarship ";
+  }
+  else
+  {
+    std::cout<<"\n  You will not get scholarship ";
+  }
+}*/
