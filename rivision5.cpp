78ffb298d963a48f5ae28e289d91b2fc230e9ce2
Aryanparashara <aryanparashara01@gmail.com>
2025-09-09 21:18:53 +0530

    Create rivision5.cpp

---
+++ rivision5.cpp
@@ -0,0 +1,63 @@
+/*#include<iostream>
+int main()
+{
+  int a,b,c,d,e,f,choice;
+  std::cout<<"\n shoice 1 is sum,choice 2 is sub,choice 3 is product,choice 4 is division ";
+  std::cout<<"\n enter the choice ";
+  std::cin>>choice;
+  if(choice==1)
+  {
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    c=a+b;
+    std::cout<<"\n sum is "<<c;
+  }
+  else if (choice==2)
+  {
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    d=a-b;
+    std::cout<<"\n sub is "<<d;
+  }
+  else if (choice==3)
+  {
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    e=a*b;
+    std::cout<<"\n product is "<<e;
+  }
+  else if (choice==4)
+  {
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    f=a/b;
+    std::cout<<"\n division is "<<f;
+  }
+  else 
+  {
+    std::cout<<"\n choose b/w 1,2,3,4 ";
+  }
+}*/
+/*#include<iostream>
+int main()
+{
+  int unit,bill;
+  std::cout<<"\n enter the unit ";
+  std::cin>>unit;
+  std::cout<<"\n monthly charges will be 5000 rupees ";
+  if ((unit>=0)&&(unit<=100))
+  {
+    bill=(unit*5)+550;
+    std::cout<<"\n bill will be"<<bill;
+  }
+  else if ((unit>=100)&&(unit<=300))
+  {
+    bill=(unit-100)*8+(100*5)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+  else 
+  {
+    bill=(unit-300)*11+(100*5)+(200*8)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+}*/
