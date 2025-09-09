f9228cc41a8f1ab510267611765a290ebe920e4a
Aryanparashara <aryanparashara01@gmail.com>
2025-09-09 21:19:14 +0530

    Create rivision6.cpp

---
+++ rivision6.cpp
@@ -0,0 +1,104 @@
+/*#include<iostream>
+int main()
+{
+  int a,b,c,d,e,f,ch;
+  std::cout<<"\n 1.add ";
+  std::cout<<"\n 2.sub ";
+  std::cout<<"\n 3.product ";
+  std::cout<<"\n 4.division ";
+  std::cout<<"\n 5.exit ";
+  std::cout<<"\n enter your choice ";
+  std::cin>>ch;
+  switch(ch)
+  {
+    case 1:
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    c=a+b;
+    std::cout<<"\n sum is "<<c;
+    break;
+    case 2:
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    d=a-b;
+    std::cout<<"\n sub is "<<d;
+    break;
+    case 3:
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    e=a*b;
+    std::cout<<"\n product is "<<e;
+    break;
+    case 4:
+    std::cout<<"\n enter 2 no.s ";
+    std::cin>>a>>b;
+    f=a/b;
+    std::cout<<"\n division is "<<f;
+    break;
+    case 5:
+    exit(0);
+    break;
+    default:
+    std::cout<<"\n invalid choice ";
+    std::cout<<"\n choose b/w 1,2,3,4,5 ";
+    break;
+  }
+}*/
+/*#include<iostream>
+int main()
+{
+  int aoc,r,h,p,t,si,voc,s,c,n,ch;
+  std::cout<<"\n enter 1.area of circle ";
+  std::cout<<"\n enter 2.simple interest ";
+  std::cout<<"\n enter 3.volume of cone ";
+  std::cout<<"\n enter 4.cube of any no. ";
+  std::cout<<"\n enter 5.square of any no. ";
+  std::cout<<"\n enter 6.exit ";
+  std::cout<<"\n enter your choice ";
+  std::cin>>ch;
+  switch(ch)
+  {
+    case 1:
+    std::cout<<"\n enter the radius ";
+    std::cin>>r;
+    aoc=(3.14*r*r);
+    std::cout<<"\n area of circle is "<<aoc;
+    break;
+    case 2:
+    std::cout<<"\n enter the principle amount ";
+    std::cin>>p;
+    std::cout<<"\n enter the rate ";
+    std::cin>>r;
+    std::cout<<"\n enter the time ";
+    std::cin>>t;
+    si=(p*r*t)/100;
+    std::cout<<"\n simple interest is "<<si;
+    break;
+    case 3:
+    std::cout<<"\n enter the radius ";
+    std::cin>>r;
+    std::cout<<"\n enter the height ";
+    std::cin>>h;
+    voc=(3.14*r*r*h)/3;
+    std::cout<<"\n volume of cone is "<<voc;
+    break;
+    case 4:
+    std::cout<<"\n enter the number ";
+    std::cin>>n;
+    c=n*n*n;
+    std::cout<<"\n cube is "<<c;
+    break;
+    case 5:
+    std::cout<<"\n enter the number ";
+    std::cin>>s;
+    s=n*n;
+    std::cout<<"\n square is "<<s;
+    break;
+    case 6:
+    exit(0);
+    break;
+    default:
+    std::cout<<"\n invalid choice ";
+    std::cout<<"\n choose b/w 1,2,3,4,5,6 ";
+  }
+}*/
