754b945461f08f308ab88c01ce0ae7cb669d471c
Aryanparashara <aryanparashara01@gmail.com>
2025-09-10 21:01:43 +0530

    Create rivision8.cpp

---
+++ rivision8.cpp
@@ -0,0 +1,101 @@
+/*#include<iostream>
+int main()
+{
+  char color;
+  std::cout<<"\n enter the traffic light color (r for red ,y for yellow ,g for green ): ";
+  std::cin>>color;
+  switch(color)
+  {
+    case 'R':
+    case 'r':
+    std::cout<<"\n Stop! the light is red ";
+    break;
+    case 'Y':
+    case 'y':
+    std::cout<<"\n Wait! the light is yellow ";
+    break;
+    case 'G':
+    case 'g':
+    std::cout<<"\n Go! the light is green ";
+    break;
+    default:
+    std::cout<<"\n Invalid! Enter R,Y,G ";
+    break;
+  }
+}*/
+/*#include<iostream>
+#include<cstdlib>
+int main()
+char n;
+std::cout<<"\n enter 1 for monday ";
+std::cout<<"\n enter 2 for tuesday ";
+std::cout<<"\n enter 3 for wednesday ";
+std::cout<<"\n enter 4 for thursday ";
+std::cout<<"\n enter 5 for friday ";
+std::cout<<"\n enter 6 for saturday ";
+std::cout<<"\n enter 7 for sunday ";
+std::cout<<"\n enter the number ";
+std::cin>>n;
+switch(n)
+{
+  case 1:
+  std::cout<<"\n The day is monday ";
+  break;
+  case 2:
+  std::cout<<"\n The day is tuesday ";
+  break;
+  case 3:
+  std::cout<<"\n The day is wednesday ";
+  break;
+  case 4:
+  std::cout<<"\n The day is thursday ";
+  break;
+  case 5:
+  std::cout<<"\n The day is friday ";
+  break;
+  case 6:
+  std::cout<<"\n The day is saturday ";
+  break;
+  case 7:
+  std::cout<<"\n The day is sunday ";
+  break;
+  case 8:
+  exit(0);
+  break;
+  default:
+  std::cout<<"\n choose b/w 1,2,3,4,5,6,7,8 ";
+  break;
+}*/
+/*#include<iostream>
+int main()
+{
+  char alphabet;
+  std::cout<<"\n enter the alphabet ";
+  std::cin>>alphabet;
+  switch(alphabet)
+  {
+    case 'A':
+    case 'a':
+    std::cout<<"\n It is a vowel ";
+    break;
+    case 'E':
+    case 'e':
+    std::cout<<"\n It is a vowel ";
+    break;
+    case 'I':
+    case 'i':
+    std::cout<<"\n It is a vowel ";
+    break;
+    case 'O':
+    case 'o':
+    std::cout<<"\n It is a vowel ";
+    break;
+    case 'U':
+    case 'u':
+    std::cout<<"\n It is a vowel ";
+    break;
+    default:
+    std::cout<<"\n It is a consonant ";
+    break;
+  }
+}*/
