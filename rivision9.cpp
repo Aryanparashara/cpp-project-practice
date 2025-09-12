f17dd35ae1e035e0e655fcfe5a44ec3b12f2439b
Aryanparashara <aryanparashara01@gmail.com>
2025-09-12 10:43:19 +0530

    Create rivision9.cpp

---
+++ rivision9.cpp
@@ -0,0 +1,90 @@
+/*#include<iostream>
+int main()
+{
+  char c;
+  std::cout<<"\n enter a character ";
+  std::cin>>c;
+  if((c>='a')&&(c<='z'))
+  {
+    std::cout<<"\n It is a small alphabet ";
+  }
+  else if ((c>='A')&&(c<='Z'))
+  {
+    std::cout<<"\n It is a capital letter ";
+  }
+  else if ((c>='0')&&(c<='9'))
+  {
+    std::cout<<"\n It is a number key ";
+  }
+  else 
+  {
+    std::cout<<"\n It is a special key ";
+  }
+}*/
+/*#include<iostream>
+int main()
+{
+  float fh,cl;
+  int ch;
+  std::cout<<"\n 1.convert temperature from fahrenheit to celsius ";
+  std::cout<<"\n 2.convert temprature from celscius to fahrenheit ";
+  std::cout<<"\n enter your choice ";
+  std::cin>>ch;
+  if(ch==1)
+  {
+    std::cout<<"\n enter temprature in fahrenheit ";
+    std::cin>>fh;
+    cl=(fh-32)/1.8;
+    std::cout<<"\n temprature in celscius "<<cl;
+  }
+  else if (ch==2)
+  {
+    std::cout<<"\n enter temprature in celsius ";
+    std::cin>>cl;
+    fh=(cl*1.8)+32;
+    std::cout<<"\n temprature in fahrenheit "<<fh;
+  }
+  else 
+  {
+    std::cout<<"\n Invalid choice ";
+  }
+  return 0;
+}*/
+/*#include<iostream>
+int main()
+{
+  int n;
+  std::cout<<"\n enter the number ";
+  std::cin>>n;
+  if((n>=65)&&(n<=90))
+  {
+    std::cout<<"\n it is a capital alphabet ";
+  }
+  else if ((n>=97)&&(n<=122))
+  {
+    std::cout<<"\n it is a small alphabet ";
+  }
+  else if ((n>=48)&&(n<=57))
+  {
+    std::cout<<"\n it is a number key ";
+  }
+  else 
+  {
+    std::cout<<"\n it is an special key ";
+  }
+}*/
+/*#include<iostream>
+int main()
+{
+  int n;
+  std::cout<<"\n enter the number ";
+  std::cin>>n;
+  if(n<0)
+  {
+    std::cout<<"\n it is a negative number ";
+  }
+  else 
+  {
+    std::cout<<"\n it is a positive number ";
+  }
+}*/
