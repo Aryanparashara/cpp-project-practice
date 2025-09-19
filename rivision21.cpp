704f17d709fe5929b50ef1f298faf4239f250a31
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:13:01 +0530

    Create rivision21.cpp

---
+++ rivision21.cpp
@@ -0,0 +1,25 @@
+/*#include<iostream>
+int main()
+{
+  int n,i,flag=0;
+  std::cout<<"\n enter a positive number: ";
+  std::cin>>n;
+  if((n==0)&&(n==1)) // 0 and 1 not prime numbers 
+  flag=1;
+  for(i=2;i<=n/2;i++)
+  {
+    if(n%i==0)
+    {
+      flag=1;
+      break;
+    }
+  }
+  if(flag==0)
+  {
+  std::cout<<"\n It is a prime number "<<n;
+  }
+  else 
+  {
+  std::cout<<"\n It is not a prime number "<<n;
+  }
+}*/
