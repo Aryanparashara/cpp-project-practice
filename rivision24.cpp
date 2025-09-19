54cb55446ca74558536537d1b0e3f8d8f8d8d981
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:14:25 +0530

    Create rivision24.cpp

---
+++ rivision24.cpp
@@ -0,0 +1,10 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+  char fname[20],mname[20],lname[20];
+  cout<<"\n Enter first,middle and last name ";
+  cin>>fname>>mname>>lname;
+  cout<<"\n Abbreviated name ";
+  cout<< "<<fname[0]<<","<<mname[0]<<","<<lname[0]<<";
+}*/
