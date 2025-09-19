d81032a55fb9796394d2fbf51e5dbac6a374300f
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:07:56 +0530

    Create rivision20.cpp

---
+++ rivision20.cpp
@@ -0,0 +1,34 @@
+/*#include<iostream>
+int a,b;
+void number()
+{
+  std::cout<<"\n Enter 2 no.s ";
+  std::cin>>a>>b;
+}
+int main()
+{
+  int c;
+  number();
+  c=a;
+  a=b;
+  b=c;
+  std::cout<<"\n a= "<<a;
+  std::cout<<"\n b= "<<b;
+}*/
+/*#include<iostream>
+int a,b;
+void number()
+{
+  std::cout<<"\n enter 2 no.s ";
+  std::cin>>a>>b;
+}
+int main()
+{
+  int c;
+  number();
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  std::cout<<"\n a= "<<a;
+ std::cout<<"\n b= "<<b;
+}*/
