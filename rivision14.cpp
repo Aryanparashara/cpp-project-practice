ff0339f761f29fc448c685d0e954988aec9f5df8
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:05:27 +0530

    Create rivision14.cpp

---
+++ rivision14.cpp
@@ -0,0 +1,34 @@
+/*#include<iostream>
+int simpleinterest(int x,int y,int z)
+{
+  int si;
+  si=(x*y*z)/100;
+  std::cout<<"\n si is "<<si;
+}
+int main()
+{
+  int p,r,t;
+  std::cout<<"\n enter the principle amount ";
+  std::cin>>p;
+  std::cout<<"\n enter the rate ";
+  std::cin>>r;
+  std::cout<<"\n enter the time ";
+  std::cin>>t;
+  simpleinterest(p,r,t);
+  return 0;
+}*/
+/*#include<iostream>
+int areaofcircle(int x)
+{
+  int aoc;
+  aoc=(3.14*x*x);
+  std::cout<<"\n area of circle is "<<aoc;
+}
+int main()
+{
+  int r;
+  std::cout<<"\n enter the radius ";
+  std::cin>>r;
+  areaofcircle(r);
+  return 0;
+}*/
