a8266af8c380c6b69f104e47b08ec0ca4579f2b7
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:05:51 +0530

    Create rivision15.cpp

---
+++ rivision15.cpp
@@ -0,0 +1,40 @@
+/*#include<iostream>
+int evenodd(int x)
+{
+  if(x%2==0)
+  {
+    std::cout<<"\n even ";
+  }
+  else 
+  {
+    std::cout<<"\n odd ";
+  }
+}
+int main()
+{
+  int x;
+  std::cout<<"\n enter the no. ";
+  std::cin>>x;
+  evenodd(x);
+  return 0;
+}*/
+/*#include<iostream>
+int evenodd1();
+void main()
+{
+  evenodd1();
+}
+ int evenodd1()
+{
+  int a;
+  std::cout<<"\n enter the number ";
+  std::cin>>a;
+  if(a%2==0)
+  {
+    std::cout<<"\n even ";
+  }
+  else 
+  {
+    std::cout<<"\n odd ";
+  }
+}*/
