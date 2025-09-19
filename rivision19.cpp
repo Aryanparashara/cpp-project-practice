039d1635690aade08f92d0c8149b5022e77ad87b
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:07:32 +0530

    Create rivision19.cpp

---
+++ rivision19.cpp
@@ -0,0 +1,56 @@
+/*#include<iostream>
+electricity();
+int main()
+{
+  electricity();
+}
+electricity()
+{
+  int unit,bill;
+  std::cout<<"\n enter the unit ";
+  std::cin>>unit;
+  std::cout<<"\n monthly charges will be 550rupees ";
+  if((unit>=0)&&(unit<=100))
+  {
+    bill=(unit*5)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+  else if ((unit>=100)&&(unit<=300))
+  {
+    bill=(unit-100)*8+(100*5)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+  else 
+  {
+    bill=(unit-300)*11+(100*5)+(200*8)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+}*/
+/*#include<iostream>
+int unit;
+void electricity()
+{
+  std::cout<<"\n enter the unit ";
+  std::cin>>unit;
+}
+void main()
+{
+  int bill;
+  std::cout<<"\n monthly charges will be 550 rupees ";
+  electricity();
+  if((unit>=0)&&(unit<=100))
+  {
+    bill=(unit*5)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+  else if ((unit>=100)&&(unit<=300))
+  {
+    bill=(unit-100)*8+(100*5)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+  else 
+  {
+    bill=(unit-300)*11+(100*5)+(200*8)+550;
+    std::cout<<"\n bill will be "<<bill;
+  }
+}*/
