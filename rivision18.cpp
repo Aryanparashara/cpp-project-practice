931822a3d6558f29793ee150c505b91b1471c6fd
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:07:02 +0530

    Create rivision18.cpp

---
+++ rivision18.cpp
@@ -0,0 +1,47 @@
+/*#include<iostream>
+#include<stdlib.h>
+number();
+int main()
+{
+  number();
+}
+number()
+{
+  int a,b,c,d,e,f,ch;
+  std::cout<<"\n enter 1 for addition ";
+  std::cout<<"\n enter 2 for subtraction ";
+  std::cout<<"\n enter 3 for product ";
+  std::cout<<"\n enter 4 for division ";
+  std::cout<<"\n enter 5 for exit ";
+  std::cout<<"\n enter your choice ";
+  std::cin>>ch;
+  switch(ch)
+  {
+    case 1:
+    std::cout<<"\n enter two numbers ";
+    std::cin>>a>>b;
+    c=a+b;
+    std::cout<<"\n sum is "<<c;
+    case 2:
+    std::cout<<"\n enter two numbers ";
+    std::cin>>a>>b;
+    d=a-b;
+    std::cout<<"\n sub is "<<d;
+    case 3:
+    std::cout<<"\n enter two numbers ";
+    std::cin>>a>>b;
+    e=a*b;
+    std::cout<<"\n product is "<<e;
+    case 4:
+    std::cout<<"\n enter two numbers ";
+    std::cin>>a>>b;
+    f=a/b;
+    std::cout<<"\n division is "<<f;
+    case 5:
+    exit(0);
+    break;
+    default:
+    std::cout<<"\n Invalid choice ";
+    std::cout<<"\n enter 1,2,3,4,5 ";
+  }
+}*/
