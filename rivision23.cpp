3cb7662e30855abe5784143e11735e2704b0cfc9
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:13:54 +0530

    Create rivision23.cpp

---
+++ rivision23.cpp
@@ -0,0 +1,15 @@
+/*#include<iostream>
+void main(void)
+{
+  int num,sum=0,rem;
+  std::cout<<"\n Enter a number ";
+  std::cin>>num;
+  // keep diving until the number is not zero 
+  while(num!=0)
+  {
+    rem=num%10;
+    sum=sum+rem;
+    num=num/10;
+  }
+  std::cout<<"\n sum of digits of the numbers is "<<sum;
+}*/
