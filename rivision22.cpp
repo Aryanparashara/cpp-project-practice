52edaf911d89cb19b06abb4ecda61be46d58cac3
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:13:24 +0530

    Create rivision22.cpp

---
+++ rivision22.cpp
@@ -0,0 +1,15 @@
+/*#include<iostream>
+int main()
+{
+  int n,reverse=0,remainder,original;
+  std::cout<<"\n Enter an Integer ";
+  std::cin>>n;
+  original=n;
+  while(n!=0)
+  {
+    remainder=n%10;
+    reverse=reverse*10+remainder;
+    n=n/10;
+  }
+  std::cout<<"\n Reversed number = "<<reverse;
+}*/
