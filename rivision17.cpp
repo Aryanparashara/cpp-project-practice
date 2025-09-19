b845c72f7dbdb9faa5077ff2e678a23861fc7bae
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:06:37 +0530

    Create rivision17.cpp

---
+++ rivision17.cpp
@@ -0,0 +1,25 @@
+/*#include<iostream>
+int add(int,int);
+float average(int,int);
+int main()
+{
+  int n1,n2,sum;
+  float avg;
+  std::cout<<"\n enter two numbers ";
+  std::cin>>n1>>n2;
+  std::cout<<"\n number 1: "<<n1;
+  std::cout<<"\n number 2: "<<n2;
+  sum=add(n1,n2);
+  avg=average(n1,n2);
+  std::cout<<"\n sum: "<<sum;
+  std::cout<<"\n average: "<<avg;
+  return 0;
+}
+int add(int x,int y)
+{
+  return x+y;
+}
+float average(int x,int y)
+{
+  return((float)(x)+(float)(y))/2;
+}*/
