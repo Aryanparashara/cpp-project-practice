80b40c374d541eec7dfa0922ce9fb0e7eb45c9d8
Aryanparashara <aryanparashara01@gmail.com>
2025-09-19 22:04:49 +0530

    Create rivision13.cpp

---
+++ rivision13.cpp
@@ -0,0 +1,26 @@
+/*#include<math.h>
+#include<iostream>
+int main()
+{
+  int no,p,result;
+  std::cout<<"\n enter the number ";
+  std::cin>>no;
+  std::cout<<"\n enter its power ";
+  std::cin>>p;
+  result=pow(no,p);
+  std::cout<<"\n answer is "<<result;
+}*/
+/*#include<iostream>
+int add(int a,int b)
+{
+  int sum=0;
+  sum=a+b;
+  std::cout<<"\n the result is "<<sum;
+}
+int main()
+{
+  int num1,num2;
+  std::cout<<"\n enter 2 values ";
+  std::cin>>num1>>num2;
+  add(num1,num2);
+}*/
