c6e441efe69bdee59513b416fa384f3d5367e16a
Aryanparashara <aryanparashara01@gmail.com>
2025-09-20 20:28:20 +0530

    Create rivision25.cpp

---
+++ rivision25.cpp
@@ -0,0 +1,22 @@
+/*#include<iostream>
+int main()
+{
+  int cp,sp,amt;
+  std::cout<<"\n Enter the cost price: ";
+  std::cin>>cp;
+  std::cout<<"\n Enter the selling price: ";
+  std::cin>>sp;
+  if(sp>cp)
+  {
+    amt=sp-cp;
+    std::cout<<"\n profit = "<<amt;
+  }
+  else if(sp<cp)
+  {
+    std::cout<<"\n Loss = "<<amt;
+  }
+  else
+  {
+    std::cout<<" No profit,No Loss";
+  }
+}*/
