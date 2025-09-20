f8edab37eeddb7cb0919e5b3fd505c832f760758
Aryanparashara <aryanparashara01@gmail.com>
2025-09-20 20:28:45 +0530

    Create rivision26.cpp

---
+++ rivision26.cpp
@@ -0,0 +1,20 @@
+/*#include<stdio.h>
+#include<iostream>
+#include<string.h>
+void main()
+{
+  char a[20];
+  std::cout<<"\n Enter any string ";
+  gets(a);
+  std::cout<<"\n Reverse of string is "<<strrev(a)<<end1;
+}*/
+/*#include<stdio.h>
+#include<iostream>
+#include<string.h>
+void main()
+{
+  char a[20];
+  std::cout<<"\n Enter any string ";
+  gets(a);
+  std::cout<<"\n length of string is "<<strlen(a)<<end1;
+}*/
