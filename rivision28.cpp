0351fc88f3b71b818b42cae1f99df6e298a0748f
Aryanparashara <aryanparashara01@gmail.com>
2025-09-22 20:57:46 +0530

    Create rivision28.cpp

---
+++ rivision28.cpp
@@ -0,0 +1,27 @@
+/*#include<stdio.h>
+#include<iostream>
+#include<string.h>
+void main()
+{
+  char a[20],b[20];
+  std::cout<<"\n Enter the first string ";
+  gets (a);
+  std::cout<<"\n Enter the second string ";
+  gets (b);
+  std::cout<<"\n copy of string is "<<strcpy(a,b)<<end1;
+}*/
+/*#include<stdio.h>
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char a[20],b[20];
+  std::cout<<"\n Enter the first string ";
+  gets(a);
+  std::cout<<"\n Enter the second string ";
+  gets(b);
+  if(strcmp(a,b)==0)
+  std::cout<<"\n They are equal ";
+  else 
+  std::cout<<"\n Not equal ";
+}*/
